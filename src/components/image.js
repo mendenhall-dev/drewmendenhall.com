5e3da63a8c064e3d215923f1b935c166edffc513
Drew Mendenhall <drew@mendenhall.io>
2019-03-15 10:45:16 -0500

    Remove single-line bracket spacing.

--- src/components/image.js
+++ src/components/image.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
+import {StaticQuery, graphql} from 'gatsby'
 import Img from 'gatsby-image'
 
 /*
@@ -17,7 +17,7 @@ const Image = () => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+        placeholderImage: file(relativePath: {eq: "gatsby-astronaut.png"}) {
           childImageSharp {
             fluid(maxWidth: 300) {
               ...GatsbyImageSharpFluid
