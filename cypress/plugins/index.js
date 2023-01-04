346e548bc89dd0f6d5ab8f8f53317b0be191d9cb
Drew Mendenhall <drew@mendenhall.io>
2023-01-04 15:30:44 -0700

    Remove Cypress

--- cypress/plugins/index.js
+++
@@ -1,8 +0,0 @@
-const dotenvPlugin = require('cypress-dotenv')
-const {initPlugin} = require('cypress-plugin-snapshots/plugin')
-
-module.exports = (on, config) => {
-  initPlugin(on, config)
-
-  return dotenvPlugin(config)
-}
