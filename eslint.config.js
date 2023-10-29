e6e69c525e3892f2a3ac8fa63cfc313e77d38ad1
Drew Mendenhall <drew@mendenhall.io>
2023-10-29 16:38:44 -0600

    Use Correct Indentation

--- eslint.config.js
+++ eslint.config.js
@@ -2,45 +2,47 @@ import typescriptESLint from '@typescript-eslint/eslint-plugin'
 import typescriptParser from '@typescript-eslint/parser'
 
 /** @type { import("eslint").Linter.FlatConfig[] } */
-const eslintConfig = [{
-  files: [
-    '**/.js',
-    '**/.ts',
-    '**/.tsx',
-  ],
-  ignores: [
-    'out/**',
-  ],
-  languageOptions:{
-    ecmaVersion: 'latest',
-  },
-  rules: {
-    'array-bracket-spacing': 'warn',
-    'comma-dangle':
-    [
-      'warn',
-      {
-        arrays: 'always-multiline',
-        exports: 'always-multiline',
-        functions: 'always-multiline',
-        imports: 'always-multiline',
-        objects: 'always-multiline',
-      },
+const eslintConfig = [
+  {
+    files: [
+      '**/.js',
+      '**/.ts',
+      '**/.tsx',
     ],
-    'comma-spacing': 'warn',
-    'curly': ['warn', 'multi-line'],
-    'indent': ['warn', 2, {SwitchCase: 1, flatTernaryExpressions: true}],
-    'no-irregular-whitespace': 'error',
-    'no-mixed-spaces-and-tabs': 'warn',
-    'no-undef': 'error',
-    'no-unused-vars': ['warn', {ignoreRestSiblings: true}],
-    'object-curly-spacing': 'warn',
-    'object-shorthand': 'warn',
-    'quotes': ['warn', 'single', {avoidEscape: true}],
-    'quote-props': ['warn', 'consistent-as-needed'],
-    'semi': ['error', 'never'],
-    'strict': ['error', 'never'],
-  },},
+    ignores: [
+      'out/**',
+    ],
+    languageOptions:{
+      ecmaVersion: 'latest',
+    },
+    rules: {
+      'array-bracket-spacing': 'warn',
+      'comma-dangle':
+      [
+        'warn',
+        {
+          arrays: 'always-multiline',
+          exports: 'always-multiline',
+          functions: 'always-multiline',
+          imports: 'always-multiline',
+          objects: 'always-multiline',
+        },
+      ],
+      'comma-spacing': 'warn',
+      'curly': ['warn', 'multi-line'],
+      'indent': ['warn', 2, {SwitchCase: 1, flatTernaryExpressions: true}],
+      'no-irregular-whitespace': 'error',
+      'no-mixed-spaces-and-tabs': 'warn',
+      'no-undef': 'error',
+      'no-unused-vars': ['warn', {ignoreRestSiblings: true}],
+      'object-curly-spacing': 'warn',
+      'object-shorthand': 'warn',
+      'quotes': ['warn', 'single', {avoidEscape: true}],
+      'quote-props': ['warn', 'consistent-as-needed'],
+      'semi': ['error', 'never'],
+      'strict': ['error', 'never'],
+    },
+  },
 
   {
     files: [
