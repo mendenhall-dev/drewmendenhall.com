6e5250c7ffae67274d920aa895c01c72e3171b3a
Drew Mendenhall <drew@mendenhall.io>
2023-10-29 13:39:00 -0600

    Fix Indentation

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,18 @@
 /** @type { import("eslint").Linter.Config } */
 const eslintConfig = {
-env: {
-  node: true,
-},
-ignorePatterns: [
-  '/out/',
-],
-parserOptions: {
-  ecmaVersion: 2019,
-  sourceType: 'module',
-},
-rules: {
-  'array-bracket-spacing': 'warn',
-  'comma-dangle':
+  env: {
+    node: true,
+  },
+  ignorePatterns: [
+    '/out/',
+  ],
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+  },
+  rules: {
+    'array-bracket-spacing': 'warn',
+    'comma-dangle':
     [
       'warn',
       {
@@ -23,22 +23,22 @@ rules: {
         objects: 'always-multiline',
       },
     ],
-  'comma-spacing': 'warn',
-  'curly': ['warn', 'multi-line'],
-  'indent': ['warn', 2, {SwitchCase: 1, flatTernaryExpressions: true}],
-  'no-irregular-whitespace': 'error',
-  'no-mixed-spaces-and-tabs': 'warn',
-  'no-undef': 'error',
-  'no-unused-vars': ['warn', {ignoreRestSiblings: true}],
-  'object-curly-spacing': 'warn',
-  'object-shorthand': 'warn',
-  'quotes': ['warn', 'single', {avoidEscape: true}],
-  'quote-props': ['warn', 'consistent-as-needed'],
-  'semi': ['error', 'never'],
-  'strict': ['error', 'never'],
-},
+    'comma-spacing': 'warn',
+    'curly': ['warn', 'multi-line'],
+    'indent': ['warn', 2, {SwitchCase: 1, flatTernaryExpressions: true}],
+    'no-irregular-whitespace': 'error',
+    'no-mixed-spaces-and-tabs': 'warn',
+    'no-undef': 'error',
+    'no-unused-vars': ['warn', {ignoreRestSiblings: true}],
+    'object-curly-spacing': 'warn',
+    'object-shorthand': 'warn',
+    'quotes': ['warn', 'single', {avoidEscape: true}],
+    'quote-props': ['warn', 'consistent-as-needed'],
+    'semi': ['error', 'never'],
+    'strict': ['error', 'never'],
+  },
 
-overrides: [{
+  overrides: [{
     files: [
       '*.ts',
       '*.tsx',
@@ -53,7 +53,7 @@ overrides: [{
     extends: [
       'plugin:@typescript-eslint/strict-type-checked',
     ],
-}],
+  }],
 }
 
 module.exports = eslintConfig
