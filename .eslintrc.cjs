05bed500560288df3967278a1170e8f04c12ef42
Drew Mendenhall <drew@mendenhall.io>
2023-10-29 16:40:58 -0600

    Migrate to Valid JS Add Typing

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,45 +1,59 @@
-env:
-  node: true
-ignorePatterns:
-  - /out/
-parserOptions:
-  ecmaVersion: 2019
-  sourceType: module
-rules:
-  array-bracket-spacing: warn
-  comma-dangle:
+/** @type { import("eslint").Linter.Config } */
+const eslintConfig = {
+env: {
+  node: true,
+},
+ignorePatterns: [
+  '/out/',
+],
+parserOptions: {
+  ecmaVersion: 2019,
+  sourceType: 'module',
+},
+rules: {
+  'array-bracket-spacing': 'warn',
+  'comma-dangle':
     [
-      warn,
+      'warn',
       {
-        arrays: always-multiline,
-        exports: always-multiline,
-        functions: always-multiline,
-        imports: always-multiline,
-        objects: always-multiline,
+        arrays: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'always-multiline',
+        imports: 'always-multiline',
+        objects: 'always-multiline',
       },
-    ]
-  comma-spacing: warn
-  curly: [warn, multi-line]
-  indent: [warn, 2, {SwitchCase: 1, flatTernaryExpressions: true}]
-  no-irregular-whitespace: error
-  no-mixed-spaces-and-tabs: warn
-  no-undef: error
-  no-unused-vars: [warn, {ignoreRestSiblings: true}]
-  object-curly-spacing: warn
-  object-shorthand: warn
-  quotes: [warn, single, {avoidEscape: true}]
-  quote-props: [warn, consistent-as-needed]
-  semi: [error, never]
-  strict: [error, never]
+    ],
+  'comma-spacing': 'warn',
+  'curly': ['warn', 'multi-line'],
+  'indent': ['warn', 2, {SwitchCase: 1, flatTernaryExpressions: true}],
+  'no-irregular-whitespace': 'error',
+  'no-mixed-spaces-and-tabs': 'warn',
+  'no-undef': 'error',
+  'no-unused-vars': ['warn', {ignoreRestSiblings: true}],
+  'object-curly-spacing': 'warn',
+  'object-shorthand': 'warn',
+  'quotes': ['warn', 'single', {avoidEscape: true}],
+  'quote-props': ['warn', 'consistent-as-needed'],
+  'semi': ['error', 'never'],
+  'strict': ['error', 'never'],
+},
 
-overrides:
-  - files:
-      - '*.ts'
-      - '*.tsx'
-    parser: '@typescript-eslint/parser'
-    parserOptions:
-      project: true
-    plugins:
-      - '@typescript-eslint'
-    extends:
-      - 'plugin:@typescript-eslint/strict-type-checked'
+overrides: [{
+    files: [
+      '*.ts',
+      '*.tsx',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: true,
+    },
+    plugins: [
+      '@typescript-eslint',
+    ],
+    extends: [
+      'plugin:@typescript-eslint/strict-type-checked',
+    ],
+}],
+}
+
+module.exports = eslintConfig
